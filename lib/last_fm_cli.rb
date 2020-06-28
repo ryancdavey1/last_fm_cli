c2a3a9f735a926674f967b328d12ab83dac7f246
ryancdavey1 <ryancdavey1@gmail.com>
2020-06-28 01:22:53 -0600

    create initial track file, bin file, and notes file

--- lib/last_fm_cli.rb
+++ lib/last_fm_cli.rb
@@ -1,6 +1,9 @@
 require "last_fm_cli/version"
+require "./last_fm_cli/cli"
+require "./last_fm_cli/track"
 
-module LastFmCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
+
+# module LastFmCli
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
---
+++ lib/last_fm_cli/NOTES.md
@@ -0,0 +1,21 @@
+Show a list of top tracks from last.fm chart
+
+...
+
+display selected song details
+
+...
+
+Add a song to a playlist
+
+...
+
+What is a song?
+
+A song has a
+  name
+  artist
+  url
+  genre?
+  playlist
+  category
